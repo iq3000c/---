9b82f00983826094e257ea6dcf46570c5b0c60a6
Yubin Jung <79090336+iq3000c@users.noreply.github.com>
2024-10-28 20:16:26 +0900

    Title: 따옴표 출력, Time: 107ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 241028/따옴표 출력/print-quote.py
@@ -0,0 +1,4 @@
+print('He says "It\'s a really simple sentence".')
+# print("He says \"It\'s a really simple sentence\".") 이건 큰 따옴표 전체를 백슬래쉬로 감싸주고 있어서 가능
+# print("""He says "It's a really simple sentence".""")
+# print('''He says "It's a really simple sentence".''')
